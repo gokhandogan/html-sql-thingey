42dc09c38074f1d255cef9b67812ca5772ff07e4
Gökhan Doğan <unspecified>
2021-01-06 22:51:45 +0300

    nearly done

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const bodyparser = require('body-parser')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { exec } = require("child_process");
 const port = process.env.PORT || 5000
 
 app.use(cors())
@@ -11,13 +12,36 @@ app.use(bodyparser.text())
 
 app.post('/api/parse', (req, res) => {
 	console.log(req.body);
+	//select * from c:\codes
+	if(req.body.startsWith("select")){
+		let dir = req.body.split(" ")[3];
+		console.log(dir)
+		exec("dir " + dir, (error, stdout, stderr) => {
+			res.send(stdout);
+		});    
+		return;
+	}
+	//insert c:\codes\jojo.memes
+	if(req.body.startsWith("insert")){
+		let fileNameVariable = req.body.split(" ")[1];
+		//echo.> c:\codes\jojo.memes
+		console.log(fileNameVariable)
+		exec("echo.> " + fileNameVariable, (error, stdout, stderr) => {
+			res.send(fileNameVariable + " yaratıldı!!?");
+		});
+		return;
+	}
 	
-	
-	
-	
-	
-	
-    res.send("asdsad\nasdasd");
+	//remove c:\codes\jojo.memes
+	if(req.body.startsWith("delete")){
+		let fileNameVariable = req.body.split(" ")[1];
+		//del c:\codes\jojo.memes
+		console.log(fileNameVariable)
+		exec("del " + fileNameVariable, (error, stdout, stderr) => {
+			res.send(fileNameVariable + " silinebildi!!?");
+		});
+		return;
+	}
 })
 
 app.listen(port, () => {
